5e35ef27ad7c46799a64d9633ff66995755c5b24
Geoff Wilson <gmwils@spotify.com>
2013-02-11 23:19:14 +0100

    Add a sitemap & favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,8 @@
 AUTHOR = u'Geoff Wilson'
 SITENAME = u'pseudofish'
 
+PLUGINS = ['pelican.plugins.sitemap',]
+
 FEED_ATOM = 'atom.xml'
 FEED_MAX_ITEMS = 15
 
@@ -20,3 +22,20 @@ LINKS =  ()
 SOCIAL = ()
 
 DEFAULT_PAGINATION = 20
+
+FILES_TO_COPY = (('extras/robots.txt', 'robots.txt'),
+                 ('extras/favicon.ico', 'favicon.ico'),)
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
